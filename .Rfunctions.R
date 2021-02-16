aa4cbd772ecc93b51f5203ca02aa34cc1a968755
Nick Pullen <nstjhp@gmail.com>
2021-02-16 19:21:55 +0100

    Forgot to put named_group_split in the hidden env

--- .Rfunctions.R
+++ .Rfunctions.R
@@ -243,7 +243,7 @@
     )
 }
 
-named_group_split <- function(.tbl, .sep=" - ", ...) {
+.env$named_group_split <- function(.tbl, .sep=" - ", ...) {
   grouped <- group_by(.tbl, ...)
   names <- rlang::eval_bare(rlang::expr(paste(!!!group_keys(grouped), sep = .sep)))
 
